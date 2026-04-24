392147bf7d61c5dd6c9415aaa77c1d4f7c76d72c
Brunet Alexandre <brunet92@mmi-dev.unilim.fr>
2026-04-24 15:44:05 +0200

    update credentials

---
+++ app/component/movieDetail/script.js
@@ -0,0 +1,22 @@
+let templateFile = await fetch("./component/movieDetail/template.html");
+let template = await templateFile.text();
+
+let movieDetail = {};
+
+movieDetail.format = function (data) {
+  let html = template;
+
+  html = html.replaceAll("{{title}}", data.name); 
+  html = html.replaceAll("{{img}}", "../server/images/" + menu.image);
+  html = html.replaceAll("{{director}}", data.director);
+  html = html.replaceAll("{{year}}", data.year);
+  html = html.replaceAll("{{category}}", data.category);
+  html = html.replaceAll("{{min_age}}", data.min_age);
+  html = html.replaceAll("{{description}}", data.description);
+  html = html.replaceAll("{{trailer}}", data.trailer);
+
+  return html;
+};
+
+export { movieDetail };
+
